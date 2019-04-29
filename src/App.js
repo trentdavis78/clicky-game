04226d0f6e673c8822176aba864e947fe40f4bb4
Trent Davis <trendavisinc@gmail.com>
2019-04-29 14:34:37 -0500

    Add header/footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Wrapper from './components/Wrapper';
 import Header from './components/Header';
- 
-function App() {
-  return (
-    <Header />
-  );
+import Footer from './components/Footer';
+import cards from "./cards.json";
+
+class App extends Component {
+  // Setting this.state.cards to the cards json array
+  state = {
+    cards
+  };
+
+  render() {
+    return (
+      <Wrapper>
+       <Header />
+       <Footer />
+      </Wrapper>      
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer class="footer">
+        <div class="footer__bottom">
+            <p>Clicky Game!</p>
+        </div>
+    </footer>
+  );
+}
+
+export default Footer;
